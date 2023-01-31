ed5c4e95b5efe60d1cf7676ec3efe271c71a0842
gpe123 <riosmanuel1920@gmail.com>
2023-01-31 03:20:30 -0600

    Update spoti.py

--- spoti.py
+++ spoti.py
@@ -1,3 +1,5 @@
+#IGNORE THIS FILE. I used this file to do some testings.
+
 import os
 import keyboard
 import sys
@@ -8,7 +10,6 @@ from SpottoYou import SpottoYou, get_config
 
 
 
-save_path = 'C:/Users/POPOCHAN1990/Desktop'
 link = "https://www.youtube.com/watch?v="
 sp_song_link = "https://open.spotify.com/track/5CmIIBRVQWLX2uXAkuBlS8?si=23d548dc17bd45d6"
 
